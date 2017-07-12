7a988cadbb8581b8036b96975196341070bf2256
lalakai <dincy0919@gmail.com>
2017-07-12 22:09:09 +0800

    新增練習4

---
+++ Lecture01 - Examples/Example03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentManagementLibrary;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.Write("請輸入學生姓名:");
+            student.name = Console.ReadLine();
+            Console.Write("請輸入學生電話:");
+            student.phone = Console.ReadLine();
+            Console.Write("請輸入學生地址:");
+            student.address = Console.ReadLine();
+
+            Console.WriteLine(
+                "學生姓名:{0}, 電話:{1}, 住址:{2}"
+                , student.name
+                , student.phone
+                , student.address
+                );
+        }
+    }
+}
